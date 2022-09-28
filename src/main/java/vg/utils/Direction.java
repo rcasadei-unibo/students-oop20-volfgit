5cc94b8efee3dfe7dce6887e5b5265b7cf672ce1
Matteo <matteo.violani@studio.unibo.it>
2022-09-28 16:40:37 +0200

    updated direction enum with coordinates

--- src/main/java/vg/utils/Direction.java
+++ src/main/java/vg/utils/Direction.java
@@ -1,8 +1,23 @@
 package vg.utils;
 
 public enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT,
+    UP(-1,0),
+    DOWN(1,0),
+    LEFT(0,-1),
+    RIGHT(0,1);
+
+    private double x;
+    private double y;
+    private Direction(final double x, final double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getX() {
+        return x;
+    }
 }
