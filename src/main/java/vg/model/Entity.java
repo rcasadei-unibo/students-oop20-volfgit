13084f42c9f3d6b1d66f0fa3acdf6ea3c241e031
Matteo <matteo.violani@studio.unibo.it>
2022-09-28 14:58:47 +0200

    added entity map interface

---
+++ src/main/java/vg/model/Entity.java
@@ -0,0 +1,12 @@
+package vg.model;
+
+import vg.utils.Direction;
+import vg.utils.Position;
+
+public interface Entity {
+
+    void move(Direction direction);
+    Position getPosition();
+    //Shape getShape();
+
+}
