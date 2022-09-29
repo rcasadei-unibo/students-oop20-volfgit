9f08378f9dc742c25b7388060530523fa722a6bf
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-29 16:45:23 +0200

    created interface Map

--- src/main/java/vg/model/Map.java
+++ src/main/java/vg/model/Map.java
@@ -1,5 +1,12 @@
 package vg.model;
 
-public interface Map {
+import vg.model.entity.Entity;
 
+public interface Map<T> {
+    boolean isInBorder(Entity toCheck);
+
+    //TODO capire come gestire i bordi dai vettori
+    T getBorders();
+
+    double getOccupiedPercentage();
 }
