f121537290a7881e4d6cfeac4d443f9843f3024c
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-29 12:15:29 +0200

    added  class

---
+++ src/main/java/vg/model/Mosquitoes.java
@@ -0,0 +1,13 @@
+package vg.model;
+
+import vg.utils.Position;
+import vg.utils.Speed;
+
+public class Mosquitoes extends MinorEnemy {
+    Mosquitoes(Position position, Speed speed) {
+        super(position, speed);
+    }
+    public void accelerate(){
+        //TODO use Speed
+    }
+}
