87d7f6c0dc07e01b785f1bb1b4a28c0edbb9f795
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-29 12:23:19 +0200

    created BasicEnemyBolt

---
+++ src/main/java/vg/model/BasicEnemyBolt.java
@@ -0,0 +1,15 @@
+package vg.model;
+
+import vg.utils.Position;
+import vg.utils.Speed;
+
+public class BasicEnemyBolt extends EnemyBolt{
+    BasicEnemyBolt(Position position, Speed speed) {
+        super(position, speed);
+    }
+
+    @Override
+    public void afterCollisionAction(){
+        //TODO
+    }
+}
