3cfd33b4fc197dd41a194cd6e2c51d7a8e7b6ce5
Matteo <matteo.violani@studio.unibo.it>
2022-09-30 15:34:09 +0200

    added method move in  player interface

--- src/main/java/vg/model/entity/dynamicEntity/Player.java
+++ src/main/java/vg/model/entity/dynamicEntity/Player.java
@@ -1,10 +1,12 @@
 package vg.model.entity.dynamicEntity;
 
 import vg.model.Tail;
+import vg.utils.V2D;
 
 public interface Player {
 
     void decLife();
     void incLife();
+    void move(V2D newPosition);
     Tail getPlayerTail();
 }
