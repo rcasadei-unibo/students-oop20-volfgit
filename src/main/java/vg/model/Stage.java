393a35de01dbf57d7089cda6765719ba82168c0d
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-30 17:08:07 +0200

    Completed Documentation in Stage interface

--- src/main/java/vg/model/Stage.java
+++ src/main/java/vg/model/Stage.java
@@ -11,48 +11,56 @@ import java.util.Set;
 /**
  * The main model class that will control all the entities
  * on the map.
- * @param <T> : the type which defines borders data structure.
+ * @param <T> V2D : the type which defines borders data structure.
  */
 public interface Stage<T> {
     /**
      * Return the current map state.
-     * @return : Map<T> in his current state.
+     * @return The current state of the map.
+     * @see Map
      */
     Map<T> getMap();
 
     /**
      * Sets the map of the stage.
-     * @param map : the map to set to.
+     * @param map The map to set to.
+     * @see Map
      */
     void setMap(Map<T> map);
 
     /**
      * Return the current player state.
-     * @return : the current player state.
+     * @return Player : the current player state.
+     * @see Player
      */
     Player getPlayer();
 
     /**
      * Sets the player in this stage.
-     * @param player : the player to set to.
+     * @param player The player to set to.
+     * @see Player
      */
     void setPlayer(Player player);
 
     /**
      * Return a Set of all static entities on the map.
-     * @return : Set<StaticEntity> that contains all staticEntity on the map.
+     * @return The Set that contains all staticEntity on the map.
+     * @see StaticEntity
      */
     Set<StaticEntity> getStaticEntitySet();
 
     /**
      * Return a Set of all dynamic entities on the map.
-     * @return : Set<DynamicEntity> that contains all staticEntity on the map.
+     * @return The Set that contains all staticEntity on the map.
+     * @see DynamicEntity
      */
     Set<DynamicEntity> getDynamicEntitySet();
 
     /**
      * Return a Set of all entities that will be deleted from the map before the next iteration begins.
      * @return : Set<AbstractEntity> that contains all entities on the map that will be deleted.
+     * @see Entity
+     * @see AbstractEntity
      */
     Set<AbstractEntity> getToDestroySet();
 
@@ -60,24 +68,65 @@ public interface Stage<T> {
      * Return a data structure containing the current actual borders of the map.
      * This will not include Magic Boxes even if they are present on the map and
      * entities con collide with.
-     * @return T : a data structure containing the borders of the map.
+     * @return The Set of T, a data structure containing the borders of the map.
      */
-    T getBorders();
+    Set<T> getBorders();
 
-    /***********************************************TODO da qui in poi devo finire di mettere
-     * altra documentazione, in più creare il metodo Map<T, Pair<MassTier,FactionType>> getCollidble()
+    /**
+     * Return all entities on the map.
+     * @return The set that contains all entities on the map.
+     * @see Entity
+     * @see AbstractEntity
      */
     Set<Entity> getAllEntities();
 
+    /**
+     * For each dynamic entity on the map the method move will be called.
+     * @see DynamicEntity
+     */
     void moveAll();
 
+    /**
+     * Add an entity to a list of entities that will be destroyed
+     * at the end of current cycle. Usually after moving entities,
+     * if they collide with entities of opposite faction then this
+     * method will be called.
+     * @param toDestroy AbstractEntity : the entity to destroy.
+     */
     void addToDestroy(AbstractEntity toDestroy);
 
+    /**
+     * After moving all the dynamic entities of the map this checks
+     * which one are colliding and call the proper method on the
+     * entity to manage the collision.
+     * @see DynamicEntity
+     * @see DynamicEntity#afterCollisionAction()
+     */
     void checkCollisions();
 
+    /**
+     * Similar to checkCollisions but instead checks for all entities
+     * if there are some that are in invalid positions and will manage them
+     * directly.
+     */
     void checkAllOutOfBounds();
 
+    /**
+     * At the end of a cycle this method will be called and delete all
+     * the entities that needs to be destroyed.
+     * @see #addToDestroy
+     */
     void destroyAll();
 
+    /**
+     * For each entity on the map, computes the cycle and apply all the changes.
+     * @see #moveAll()
+     * @see #checkCollisions()
+     * @see #checkAllOutOfBounds()
+     * @see #destroyAll()
+     */
     void doCycle();
+
+    /*TODO in più creare il metodo Map<T, Pair<MassTier,FactionType>> getCollidble().
+     */
 }
