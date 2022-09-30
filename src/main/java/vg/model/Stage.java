31d67942a3d8aae56cdf836f1998798597c2d277
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-30 15:09:43 +0200

    added some documentation in Stage interface

--- src/main/java/vg/model/Stage.java
+++ src/main/java/vg/model/Stage.java
@@ -8,24 +8,65 @@ import vg.model.entity.staticEntity.StaticEntity;
 
 import java.util.Set;
 
+/**
+ * The main model class that will control all the entities
+ * on the map.
+ * @param <T> : the type which defines borders data structure.
+ */
 public interface Stage<T> {
+    /**
+     * Return the current map state.
+     * @return : Map<T> in his current state.
+     */
     Map<T> getMap();
 
+    /**
+     * Sets the map of the stage.
+     * @param map : the map to set to.
+     */
     void setMap(Map<T> map);
 
+    /**
+     * Return the current player state.
+     * @return : the current player state.
+     */
     Player getPlayer();
 
-    //TODO questo setter non so se serve
+    /**
+     * Sets the player in this stage.
+     * @param player : the player to set to.
+     */
     void setPlayer(Player player);
 
+    /**
+     * Return a Set of all static entities on the map.
+     * @return : Set<StaticEntity> that contains all staticEntity on the map.
+     */
     Set<StaticEntity> getStaticEntitySet();
 
+    /**
+     * Return a Set of all dynamic entities on the map.
+     * @return : Set<DynamicEntity> that contains all staticEntity on the map.
+     */
     Set<DynamicEntity> getDynamicEntitySet();
 
+    /**
+     * Return a Set of all entities that will be deleted from the map before the next iteration begins.
+     * @return : Set<AbstractEntity> that contains all entities on the map that will be deleted.
+     */
     Set<AbstractEntity> getToDestroySet();
 
+    /**
+     * Return a data structure containing the current actual borders of the map.
+     * This will not include Magic Boxes even if they are present on the map and
+     * entities con collide with.
+     * @return T : a data structure containing the borders of the map.
+     */
     T getBorders();
 
+    /***********************************************TODO da qui in poi devo finire di mettere
+     * altra documentazione, in più creare il metodo Map<T, Pair<MassTier,FactionType>> getCollidble()
+     */
     Set<Entity> getAllEntities();
 
     void moveAll();
