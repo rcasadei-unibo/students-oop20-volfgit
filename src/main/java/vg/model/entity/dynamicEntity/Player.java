9c9cabbf9cfac818da43083b2f47fd09a231add7
Matteo <matteo.violani@studio.unibo.it>
2022-09-30 16:03:05 +0200

    Player interface extend Entity

--- src/main/java/vg/model/entity/dynamicEntity/Player.java
+++ src/main/java/vg/model/entity/dynamicEntity/Player.java
@@ -1,12 +1,16 @@
 package vg.model.entity.dynamicEntity;
 
 import vg.model.Tail;
+import vg.model.bonus.BonusType;
+import vg.model.entity.AbstractEntity;
+import vg.model.entity.Entity;
 import vg.utils.V2D;
 
-public interface Player {
+public interface Player extends Entity {
 
     void decLife();
     void incLife();
     void move(V2D newPosition);
     Tail getPlayerTail();
+
 }
