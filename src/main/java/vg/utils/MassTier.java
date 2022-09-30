06ef9bffcd2d5e5a8cfbb73b6673f61969f3b351
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-30 15:31:10 +0200

    added full documentation in MassTier enum and a TODO

--- src/main/java/vg/utils/MassTier.java
+++ src/main/java/vg/utils/MassTier.java
@@ -7,5 +7,25 @@ package vg.utils;
  * a mass tier that defines entities which cannot bounce.
  */
 public enum MassTier {
-    HIGH, MEDIUM, LOW, NOCOLLISION;
+    /**
+     * HIGH : this is the mass tier for borders and bosses.
+     */
+    HIGH,
+    /**
+     * MEDIUM : this is the mass tier for minor enemy such
+     * as Mosquitoes.
+     */
+    MEDIUM,
+    /**
+     * LOW : this mass tier is not used for any Entity
+     * at the present time.
+     */
+    LOW,
+    /**
+     * NOCOLLISION : this mass tier is the lowest one
+     * and defines entities that will not bounce even if
+     * colliding with other enties in this mass tier.
+     */
+    NOCOLLISION;
+    //TODO creare un metodo che controlli se la mass tier è maggiore uguale e diversa da NOCOLLISION
 }
