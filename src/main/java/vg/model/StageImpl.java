a98070baa2d902cbe9ebc222cd5b279b0ee6f51e
j3yxz <alexandru.bragari@studio.unibo.it>
2022-09-30 19:05:08 +0200

    added doc in StageImpl

--- src/main/java/vg/model/StageImpl.java
+++ src/main/java/vg/model/StageImpl.java
@@ -17,88 +17,186 @@ import java.util.stream.Stream;
  * @see V2D
  */
 public class StageImpl implements Stage<V2D> {
+
+
+    /**
+     * The current score earned by the player.
+     */
     private int currentScore;
+    /**
+     * The player.
+     */
     private Player player;
+    /**
+     * The set of static entities.
+     * @see #getStaticEntitySet()
+     */
     private Set<StaticEntity> ss;
+    /**
+     * The set of dynamic entities.
+     * @see #getDynamicEntitySet()
+     */
     private Set<DynamicEntity> ds;
+    /**
+     * The set of entities that need to be destroyed.
+     * @see #getToDestroySet()
+     */
     private Set<AbstractEntity> toDestroy;
+    /**
+     * The current active map of the Stage. This will change as the player
+     * will progress from one level to another. The Score of the player is
+     * incremented as the player completes levels and is saved in this class.
+     * @see Map
+     */
     private Map<V2D> map;
 
+    /**
+     * Constructor with map and currentScore as parameters, useful if the
+     * Stage is created from a saved file.
+     * @param currentScore The player score
+     * @param map The map
+     * @see Map
+     * @see Player
+     * @see Stage
+     */
     public StageImpl(final int currentScore, final Map<V2D> map) {
         this.currentScore = currentScore;
         this.map = map;
     }
-
+    /**
+     * Constructor with only map as parameter, useful if the
+     * game is a new one and the currentScore will be set to 0.
+     * @param map The map
+     * @see Map
+     * @see Player
+     * @see Stage
+     */
     public StageImpl(final Map<V2D> map) {
         this.currentScore = 0;
         this.map = map;
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
+    public int getCurrentScore() {
+        return currentScore;
+    }
+    /**
+     *
+     * {@inheritDoc}
+     */
+    public void setCurrentScore(final int currentScore) {
+        this.currentScore = currentScore;
+    }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Map<V2D> getMap() {
         return map;
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
-    public void setMap(Map<V2D> map) {
+    public void setMap(final Map<V2D> map) {
         this.map = map;
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Player getPlayer() {
         return player;
     }
 
-    //TODO questo setter non so se serve
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void setPlayer(final Player player) {
         this.player = player;
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Set<StaticEntity> getStaticEntitySet() {
         return ss;
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Set<DynamicEntity> getDynamicEntitySet() {
         return ds;
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Set<AbstractEntity> getToDestroySet() {
         return toDestroy;
     }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Set<V2D> getBorders() {
         return getMap().getBorders();
     }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public Set<Entity> getAllEntities() {
         return Stream.of(getDynamicEntitySet().stream(), getStaticEntitySet().stream(), Stream.of(getPlayer())).flatMap(e -> e).collect(Collectors.toSet());
     }
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void moveAll() {
         //TODO dare al giocatore la giusta direzione (passarlo come argomento?)
         getPlayer().move(new V2D(0, 0));
         getDynamicEntitySet().forEach(e -> e.move(e.getSpeed()));
     }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void addToDestroy(final AbstractEntity toDestroy) {
         this.getToDestroySet().add(toDestroy);
     }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void checkCollisions() {
-        getDynamicEntitySet().forEach(e -> {
-            getAllEntities().forEach(t -> {
-                //TODO aggiungere la condizione per cui effettivamente stanno collidendo
-                if (!e.equals(t)) {
-                    e.afterCollisionAction();
-                }
-            });
-        });
+        getDynamicEntitySet().forEach(e -> getAllEntities().forEach(t -> {
+            //TODO aggiungere la condizione per cui effettivamente stanno collidendo
+            if (!e.equals(t)) {
+                e.afterCollisionAction();
+            }
+        }));
     }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void checkAllOutOfBounds() {
         getDynamicEntitySet().forEach(e -> {
@@ -107,6 +205,10 @@ public class StageImpl implements Stage<V2D> {
             }
         });
     }
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void destroyAll() {
         getToDestroySet().forEach(e -> {
@@ -119,8 +221,10 @@ public class StageImpl implements Stage<V2D> {
         });
         getToDestroySet().removeAll(getToDestroySet());
     }
-
-
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public void doCycle() {
         moveAll();
