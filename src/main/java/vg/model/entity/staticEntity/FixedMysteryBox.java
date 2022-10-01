386fdf6ea586ea28663082597085ed21eb2b2f0f
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-01 20:05:48 +0200

    fixed mystery box constructor updated and added doc

--- src/main/java/vg/model/entity/staticEntity/FixedMysteryBox.java
+++ src/main/java/vg/model/entity/staticEntity/FixedMysteryBox.java
@@ -1,11 +1,25 @@
 package vg.model.entity.staticEntity;
 
+import vg.model.bonus.Bonus;
+import vg.model.bonus.BonusImpl;
 import vg.model.entity.staticEntity.MysteryBox;
 import vg.utils.V2D;
 
-public class FixedMysteryBox extends MysteryBox {
+/**
+ * A simple mystery box that is always fixed in the same
+ * spot.
+ * @see vg.model.entity.staticEntity.MysteryBox
+ * @see vg.model.entity.staticEntity.StaticEntity
+ */
+public class FixedMysteryBox extends MysteryBox<BonusImpl> {
 
-    FixedMysteryBox(V2D position) {
-        super(position);
+    /**
+     * {@link MysteryBox#MysteryBox(V2D, int, Bonus)}.
+     * @param position the position of the mystery box.
+     * @param halfWidth the half-width and height of the box
+     * @param bonus the bonus contained by the box
+     */
+    FixedMysteryBox(final V2D position, final int halfWidth, final BonusImpl bonus) {
+        super(position, halfWidth, bonus);
     }
 }
