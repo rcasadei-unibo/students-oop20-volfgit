445cc5f4fad33e0091f14916da14554b10d845e8
Matteo <matteo.violani@studio.unibo.it>
2022-10-02 11:41:47 +0200

    added speedup of player

--- src/main/java/vg/model/entity/dynamicEntity/player/BasePlayer.java
+++ src/main/java/vg/model/entity/dynamicEntity/player/BasePlayer.java
@@ -1,9 +1,9 @@
-package vg.model.entity.dynamicEntity;
+package vg.model.entity.dynamicEntity.player;
 
-import vg.model.Tail;
-import vg.model.TailImpl;
-import vg.model.bonus.BonusType;
+import vg.model.entity.dynamicEntity.DynamicEntity;
 import vg.utils.V2D;
+
+import java.util.Optional;
 
 /**
  * Player Entity.*/
@@ -12,21 +12,29 @@ public final class BasePlayer extends DynamicEntity implements Player {
      * Maximum player life.
      * */
     static final int PLAYER_MAX_LIFE = 5;
+    /**
+     * Default player speed.
+     */
     static final V2D DEFAULT_PLAYER_SPEED = new V2D(1, 1);
+
     /**
      * Life of player.
-     * */
+     */
     private int life;
     /**
+     *  Alternative speed of entity. It can be bigger or smaller than actual
+     */
+    private Optional<V2D> speedUp;
+    /**
      * Tail created by player while moves in map.
-     * */
+     */
     private final Tail tail;
     //private Shield shield;
 
     /**
      * @param position starting position of player
      * @return Player with default life
-     * */
+     */
     public static BasePlayer newPlayer(final V2D position) {
         return new BasePlayer(position, PLAYER_MAX_LIFE);
     };
@@ -36,7 +44,7 @@ public final class BasePlayer extends DynamicEntity implements Player {
      * @param position starting position
      * @param life starting life of player
      * @return Player with user define position and life
-     * */
+     */
     public static BasePlayer newPlayer(final V2D position, final int life) {
         int playerLife = life < 0 || life > PLAYER_MAX_LIFE ? PLAYER_MAX_LIFE : life;
         return new BasePlayer(position, playerLife);
@@ -67,6 +75,28 @@ public final class BasePlayer extends DynamicEntity implements Player {
      */
     public Tail getPlayerTail() {
         return this.tail;
+    }
+
+    @Override
+    public void enableSpeedUp(final V2D speed) {
+        /*
+        * In order to not chnage direction of move with speed vector
+        * is checked if coordinates are < 0
+        */
+        if (this.speedUp.isEmpty() && speed.getX() >= 0 && speed.getY() > 0) {
+            this.speedUp = Optional.of(speed);
+        }
+    }
+
+    @Override
+    public void disableSpeedUp() {
+        this.speedUp = Optional.empty();
+    }
+
+    @Override
+    public V2D getSpeed() {
+        //if speedUp is set return it else return original speed
+        return this.speedUp.orElseGet(super::getSpeed);
     }
 
 }
