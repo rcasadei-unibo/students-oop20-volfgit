18dac3715c4f945885cd719231a98c29bd703ee8
Matteo <matteo.violani@studio.unibo.it>
2022-10-03 19:09:31 +0200

    fixed constructor of bolt and enemybolt

--- src/main/java/vg/core/GameEngine.java
+++ src/main/java/vg/core/GameEngine.java
@@ -1,28 +1,36 @@
 package vg.core;
 
+import vg.input.Command;
+import vg.input.CommandInvoker;
+import vg.model.Stage;
 import vg.utils.GameState;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Game Engine class, manager game loop and refresh timing
  * during gameplay
  * */
-public class GameEngine {
+public class GameEngine implements CommandInvoker {
     //TODO: save object model and view
-    //private model object
-    //private view object
+    private Stage stage;
+
+    private List<Command> movementQueue;
+
     private long period = 20; // frequencies = 1/period
     private GameState gameState = GameState.STOPPED;
 
-    public void setup(){
+
+    public void setup() {
         //TODO: init model and view in GameEngine.setup()
-        //init model
-        //init view
+        this.movementQueue = new ArrayList<>();
     }
 
-    public void gameLoop(){
+    public void gameLoop() {
         long prevCycleTime = System.currentTimeMillis();
-        //TODO: it has to be always true ?
-        while(gameState != GameState.STOPPED || gameState != GameState.PAUSED){
+
+        while (gameState != GameState.STOPPED || gameState != GameState.PAUSED) {
             long curCycleTime = System.currentTimeMillis();
             long elapsedTime = curCycleTime - prevCycleTime;
             processInput();
@@ -36,31 +44,42 @@ public class GameEngine {
     }
 
 
-    private void updateGame(long elapsedTime) {
+    private void updateGame(final long elapsedTime) {
         //TODO: call update method of model
         //world.update(elapsedTime);
     }
 
+    /**
+     * Process command in the head of queue (the older one)
+     * in order to move player
+     * */
     private void processInput() {
-        //TODO: process keyboard input
-        System.out.println("Process input");
+        if (!this.movementQueue.isEmpty()) {
+            Command cmd = this.movementQueue.get(0);
+            this.movementQueue.remove(cmd);
+            cmd.execute(this.stage.getPlayer());
+        }
     }
 
     private void render() {
+
         //view.render();
         System.out.println("render gui ");
     }
 
-    private void waitForNextFrame(long elapsedTime){
+    private void waitForNextFrame(final long elapsedTime) {
         long dt = System.currentTimeMillis() - elapsedTime;
         if (dt < period) {
             try {
-                Thread.sleep(period-dt);
-            }
-            catch (InterruptedException e) {
+                Thread.sleep(period - dt);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
-                //TODO: how to handle that exception?
             }
         }
+    }
+
+    @Override
+    public void appendMovementCommand(Command cmd) {
+        this.movementQueue.add(cmd);
     }
 }
