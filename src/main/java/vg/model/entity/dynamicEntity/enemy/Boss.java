e8e4389159933021a6135def9a0b0474153b3401
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-04 12:02:00 +0200

    updated Boss abstract constructor

--- src/main/java/vg/model/entity/dynamicEntity/enemy/Boss.java
+++ src/main/java/vg/model/entity/dynamicEntity/enemy/Boss.java
@@ -1,9 +1,11 @@
 package vg.model.entity.dynamicEntity.enemy;
 
+import vg.utils.MassTier;
+import vg.utils.Shape;
 import vg.utils.V2D;
 
 public abstract class Boss extends Enemy {
-    Boss(V2D position, V2D speed) {
-        super(position, speed);
+    Boss(final V2D position, final V2D speed, final int radius, final Shape shape, final MassTier massTier) {
+        super(position, speed, radius, shape, massTier);
     }
 }
