327fe7069d4c58a750a78c897560687e2d854f60
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-04 14:17:09 +0200

    created class for Testing MapImpl class

--- src/test/java/vg/model/MapImplTest.java
+++ src/test/java/vg/model/MapImplTest.java
@@ -1,4 +1,44 @@
+package vg.model;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MapImplTest {
-  
+
+    @Test
+    void getOccupiedPercentage() {
+    }
+
+    @Test
+    void getBorders() {
+    }
+
+    @Test
+    void updateBorders() {
+    }
+
+    @Test
+    void isTailCompleted() {
+    }
+
+    @Test
+    void toCapture() {
+    }
+
+    @Test
+    void removeEntity() {
+    }
+
+    @Test
+    void getActiveBonus() {
+    }
+
+    @Test
+    void getAllMysteryBoxes() {
+    }
+
+    @Test
+    void getFriendlyBolts() {
+    }
 }
