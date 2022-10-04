ca7c0af50dc747d4d5bad273caf08005ab53e9ff
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-04 12:06:30 +0200

    updated enemy constructors

--- src/main/java/vg/model/entity/dynamicEntity/enemy/Mosquitoes.java
+++ src/main/java/vg/model/entity/dynamicEntity/enemy/Mosquitoes.java
@@ -1,12 +1,14 @@
 package vg.model.entity.dynamicEntity.enemy;
 
+import vg.utils.MassTier;
+import vg.utils.Shape;
 import vg.utils.V2D;
 
 public class Mosquitoes extends MinorEnemy {
-    Mosquitoes(V2D position, V2D speed) {
-        super(position, speed);
+    Mosquitoes(final V2D v2D, final V2D speed, final int radius, final Shape shape, final MassTier massTier) {
+        super(v2D, speed, radius, shape, massTier);
     }
-    public void accelerate(){
+    public void accelerate() {
         //TODO use Speed
     }
 }
