7dd7c68dfaab9d1cc23f9a87fd9df5a23417c732
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-08 20:01:53 +0200

    feat(controller): work in progress in StateController interface

--- src/main/java/vg/controller/StateController.java
+++ src/main/java/vg/controller/StateController.java
@@ -1,4 +1,23 @@
 package vg.controller;
 
-public interface StateController {
+import javafx.event.Event;
+
+/**
+ * The state controller is the controller of a "state" of the software.
+ * The states can be either a game in progress, paused or menu state.
+ * @param <V> the type of view
+ */
+public interface StateController<V> {
+    /**
+     * Returns the view controlled by the controller.
+     * @return the view
+     */
+    V getView();
+
+    /**
+     * Sets the view of the controller.
+     * @param view the view that will be bound
+     */
+    void setView(V view);
+    void activatesEvent(KeyEvent e);
 }
