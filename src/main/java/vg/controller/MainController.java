c154f645f3a8b872693a62d5d0f8a7e6f125a95a
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-08 15:27:18 +0200

    feat(controller): added methods and doc for main controller interface

--- src/main/java/vg/controller/MainController.java
+++ src/main/java/vg/controller/MainController.java
@@ -1,4 +1,34 @@
 package vg.controller;
 
-public interface MainController {
+/**
+ * The main controller, that will generate the Binder and
+ * all the controller-view will communicate through it.
+ * @param <C> the type of controller
+ * @param <V> the type of view
+ */
+public interface MainController<C, V> {
+
+    /**
+     * Returns the binder.
+     * @return the binder
+     */
+    Binder<C, V> getBinder();
+
+    /**
+     * Returns the stateController.
+     * @return the stateController
+     */
+    C getStateController();
+
+    /**
+     * Returns the view.
+     * @return the view
+     */
+    V getView();
+
+    /**
+     * Sets the key bindings to the state controller.
+     * @param stateController the stateController
+     */
+    void setIOSettings(C stateController);
 }
