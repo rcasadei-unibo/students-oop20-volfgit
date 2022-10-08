a985a1fa0ab2b0ede796c9db815fbe4f6a1d095e
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-08 15:25:55 +0200

    feat(controller): added methods and doc of binder interface

--- src/main/java/vg/controller/Binder.java
+++ src/main/java/vg/controller/Binder.java
@@ -1,2 +1,25 @@
-package vg.controller;public interface Binder {
+package vg.controller;
+
+/**
+ * The binder set up communication between a controller
+ * and a view, either by creating them or passing each other
+ * calling the correct methods.
+ * @param <C> the controller
+ * @param <V> the view
+ */
+public interface Binder<C, V> {
+    /**
+     * Instantiates a controller and a view binding them
+     * to each other.
+     * @param controller the controller
+     * @param view the view
+     */
+    void init(C controller, V view);
+
+    /**
+     * Binds an existing controller to an existing view.
+     * @param controller the controller
+     * @param view the view
+     */
+    void bind(C controller, V view);
 }
