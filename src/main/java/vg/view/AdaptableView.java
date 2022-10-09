b030f35941920309ab5de5a41b701a8bb1a8db81
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-09 17:57:49 +0200

    feat(view): AdaptableView implemented with doc

--- src/main/java/vg/view/AdaptableView.java
+++ src/main/java/vg/view/AdaptableView.java
@@ -1,4 +1,18 @@
 package vg.view;
 
-public abstract class AdaptableView implements View {
+import vg.controller.MainController;
+import vg.controller.StateController;
+
+/**
+ * A view that resize and correctly puts elements on the screen
+ * based on the resolution.
+ */
+public abstract class AdaptableView implements View<StateController<AdaptableView>> {
+    /**
+     * This draw must use {@link vg.view.utils.ScreenLoader} utilities when
+     * implemented to resize correctly.
+     */
+    @Override
+    public void draw() {
+    }
 }
