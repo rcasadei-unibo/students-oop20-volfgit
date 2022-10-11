bace5bde44c8eccab52d69975e3e95fc491508aa
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2022-10-11 23:31:19 +0200

    Add GameBoard file

---
+++ src/main/java/vg/view/gameBoard/GameBoard.java
@@ -0,0 +1,21 @@
+package vg.view.gameBoard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GameBoard extends Application {
+
+    public static void main(String[] args) {
+
+//        GameBoard game = new GameBoard();
+//        game.start(new Stage());
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+}
