5bb25782e245baba68725d7e344cdbb4a0509670
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2022-10-13 00:01:02 +0200

    Add GameBoard fxml and controller

--- src/main/java/vg/view/gameBoard/GameBoard.java
+++ src/main/java/vg/view/gameBoard/GameBoard.java
@@ -8,13 +8,21 @@ import vg.utils.LoadFxmlUtils;
 
 public class GameBoard extends Application {
 
+    GameBoardController controller = new GameBoardController();
     @Override
     public void start(Stage stage) throws Exception {
         Rectangle rect = new Rectangle(100, 100, 100, 100);
         Scene scene = new Scene(LoadFxmlUtils.loadFxml("layout/GameBoard.fxml"));
-        stage.setTitle("Ti pregoooooo");
+
         stage.setScene(scene);
         stage.sizeToScene();
+
+        scene.getRoot().lookup("#gameArea").setClip(rect);
+        scene.getRoot().lookup("#gameArea").setStyle("-fx-background-color: #000000;");
+        scene.getRoot().lookup("#gameAreaDue").setStyle("-fx-background-color: #2a77ff;");
+
+        controller.stampa();
+
         stage.show();
     }
 }
--- src/main/java/vg/view/gameBoard/GameBoardController.java
+++ src/main/java/vg/view/gameBoard/GameBoardController.java
@@ -1,4 +1,32 @@
 package vg.view.gameBoard;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
 public class GameBoardController {
+
+    public BorderPane gameAreaDue;
+    @FXML
+    private Pane gameArea = new Pane();
+
+    @FXML
+    private void initialize() {
+        gameArea.setStyle("-fx-background-color: #ff08d7;");
+    }
+
+    @FXML
+    private void gameAreaClicked() {
+        System.out.println("gameAreaClicked");
+    }
+
+    public void stampa() {
+        System.out.println("stampa");
+        initialize();
+    }
+
+    public Pane getGameArea() {
+        return gameArea;
+    }
+
 }
