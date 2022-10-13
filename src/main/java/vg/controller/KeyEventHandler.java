25f637f1ae7787a9f5aace5949efe3947660b6b6
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-13 15:57:25 +0200

    fix(controller): KeyEventHandler refactored into 2 separeted classes

--- src/main/java/vg/controller/KeyEventHandler.java
+++ src/main/java/vg/controller/KeyEventHandler.java
@@ -5,34 +5,20 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Stream;
 
 import static javafx.scene.input.KeyEvent.KEY_PRESSED;
+import static javafx.scene.input.KeyEvent.KEY_RELEASED;
 
 public final class KeyEventHandler implements EventHandler<KeyEvent> {
-    /**
-     * The map the associates an action to a key.
-     */
-    private final Map<KeyCode, KeyAction> currentSettings;
-    private KeyEventHandler(final Map<KeyCode, KeyAction> settings) {
-        this.currentSettings = settings;
-    }
-    public static KeyEventHandler defaultKeyEventImpl() {
-        Map<KeyCode, KeyAction> settings = new HashMap<>();
-        Stream.of(KeyAction.values()).forEach(e -> settings.put(KeyCode.valueOf(e.toString()), e));
-        return new KeyEventHandler(settings);
-    }
-    public KeyEventHandler fromSettingsKeyEventImpl(final Map<KeyCode, KeyAction> settings) {
-        return new KeyEventHandler(settings);
-    }
+    private final KeySettings keySettings = KeySettings.defaultKeyEventImpl();
+    
     /**
      * {@inheritDoc}
      */
 
     public KeyAction keyPressed(final KeyCode k) {
-        return this.currentSettings.get(k);
+        return this.keySettings.currentSettings.get(k);
     }
 
     /**
@@ -46,12 +32,15 @@ public final class KeyEventHandler implements EventHandler<KeyEvent> {
 
     @Override
     public void handle(KeyEvent event) {
-
+       // System.out.println(event);
         if (event.getEventType().equals(KEY_PRESSED)){
            //TODO do this in a non-static way ((StateController) Binder.getController()).activatesEvent(keyPressed(event.getCode()));
             System.out.println("KEY_PRESSED" + ": " + event.getCode());
-        } else {
+        } else if (event.getEventType().equals(KEY_RELEASED)){
             System.out.println("KEY_RELEASED" + ": " + event.getCode());
+        } else {
+            //TODO this is "typed"...........
+            System.out.println(event);
         }
     }
 }
