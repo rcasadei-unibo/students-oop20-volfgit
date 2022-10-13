2a630ef1593ec0290846734047c0fb931af9d816
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-13 17:58:31 +0200

    feat(view): SceneController implemented with doc

--- src/main/java/vg/view/SceneController.java
+++ src/main/java/vg/view/SceneController.java
@@ -1,2 +1,16 @@
-package vg.view;public interface SceneController {
+package vg.view;
+
+import vg.controller.KeyAction;
+
+/**
+ * Interface for fxml controllers (single scene).
+ */
+public interface SceneController {
+    /**
+     * Based on a {@link KeyAction}, the controller
+     * will create an effect.
+     * @param k {@link KeyAction}
+     */
+    void activateEvent(KeyAction k);
+
 }
