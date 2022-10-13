b1ddf4f3d4590a2c09e762c81dc84535c174ee71
Matteo <matteo.violani@studio.unibo.it>
2022-10-13 12:34:06 +0200

    feat(viewmanager): call method of view's controller in order to apply keyboard action to domain

--- src/main/java/vg/view/ViewManager.java
+++ src/main/java/vg/view/ViewManager.java
@@ -1,15 +1,16 @@
 package vg.view;
 
 import javafx.scene.Scene;
+import vg.controller.StateController;
 
 import java.util.concurrent.ExecutionException;
 
-public interface ViewManager {
+public interface ViewManager<V> {
     /**
      * Add new scene to the stack of views and show it.
      * @param scene new scene view to be shown.
      */
-    void addScene(final Scene scene);
+    void addScene(final Scene scene, final StateController<V> controller);
 
     /**
      * Removed last-added scene view from stack and show the previous one.
--- src/main/java/vg/view/ViewManagerImpl.java
+++ src/main/java/vg/view/ViewManagerImpl.java
@@ -1,36 +1,50 @@
 package vg.view;
 
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+import vg.controller.KeyAction;
+import vg.controller.StateController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-public class ViewManagerImpl implements ViewManager {
-    private List<Scene> sceneStack;
+public class ViewManagerImpl<V> implements ViewManager<V> {
+    private final Stage stage;
+    private List<Pair<StateController<V>, Scene>> sceneStack;
 
-    private ViewManagerImpl(final Scene rootScene) {
+    private ViewManagerImpl(final Stage stage) {
+        this.stage = stage;
+        stage.addEventHandler(KeyEvent.KEY_PRESSED, e -> {
+            StateController<V> ctrl = this.sceneStack.get(this.sceneStack.size() - 1).getKey();
+            KeyAction action = ctrl.captureKeyEvent(e.getCode());
+            ctrl.activatesEvent(action);
+        });
         this.sceneStack = new ArrayList<>();
-        this.sceneStack.add(rootScene);
     }
     /**
      * Return new ViewManager with.
-     * @param root the main scene that cannot be removed, the entry point view
+     * @param stage the main scene that cannot be removed, the entry point view
      */
-    public static ViewManagerImpl ViewManagerWithRoot(final Scene root) {
-       return new ViewManagerImpl(root);
+    public ViewManagerImpl<V> ViewManagerWithRoot(final Stage stage) {
+        return new ViewManagerImpl<>(stage);
     }
 
     @Override
-    public void addScene(final Scene scene) {
-        this.sceneStack.add(scene);
+    public void addScene(final Scene scene, final StateController<V> controller) {
+        this.sceneStack.add(new Pair<>(controller, scene));
+        this.stage.setScene(scene);
     }
+
 
     @Override
     public void popScene() throws ExecutionException {
         int stackSize = this.sceneStack.size();
         if (stackSize > 1) {
-            this.sceneStack.remove(stackSize-1);
+            this.sceneStack.remove(stackSize - 1);
+            this.stage.setScene(this.sceneStack.get(stackSize - 1).getValue());
         } else {
             throw new ExecutionException("No other scene can be removed from stack!",
                     new Throwable("You are trying to removed the root scene from sceneStack"));
