3b70c49f76f9a65746a34a186d4b587480647189
Matteo <matteo.violani@studio.unibo.it>
2022-10-14 11:07:34 +0200

    fix(ViewManager): added javadoc to a getStage method

--- src/main/java/vg/view/ViewManager.java
+++ src/main/java/vg/view/ViewManager.java
@@ -9,12 +9,14 @@ public interface ViewManager {
      * Add new scene to the stack of views and show it.
      * @param scene new scene view to be shown.
      */
-    void addScene(final Scene scene/*, final EventHandlerController controller*/);
-
+    void addScene(final Scene scene);
     /**
      * Removed last-added scene view from stack and show the previous one.
      */
     void popScene();
 
+    /**
+     * @return Primary stage of application
+     */
     Stage getStage();
 }
