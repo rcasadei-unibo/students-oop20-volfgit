b306ecdd598eb6b2f7d6b8fe88681cd55cbda789
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2022-10-16 02:25:27 +0200

    Improved gameBoard layout

--- src/main/java/vg/view/gameBoard/GameBoard.java
+++ src/main/java/vg/view/gameBoard/GameBoard.java
@@ -3,6 +3,7 @@ package vg.view.gameBoard;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import vg.utils.LoadFxmlUtils;
 import vg.view.ViewManager;
@@ -21,10 +22,15 @@ public class GameBoard extends Application {
         Scene scene = new Scene(LoadFxmlUtils.loadFxml("layout/GameBoard.fxml"));
         stage.setTitle("GameBoard");
         stage.setScene(scene);
-        stage.minWidthProperty().bind(scene.heightProperty().multiply(2));
-        stage.minHeightProperty().bind(scene.widthProperty().divide(2));
-        stage.show();
 
+        stage.setMinWidth(1400);
+        stage.setMinHeight(950);
+        stage.setMaxWidth(1400);
+        stage.setMaxHeight(950);
+//        stage.minWidthProperty().bind(scene.heightProperty().multiply(2));
+//        stage.minHeightProperty().bind(scene.widthProperty().divide(2));
+
+        stage.show();
     }
 }
 
