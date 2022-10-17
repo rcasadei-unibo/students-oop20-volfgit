ae9c51f309d779528177b50e5e9a7d622bce854f
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-17 21:35:24 +0200

    fix(model): mass tier order reversed (now ascending)

--- src/main/java/vg/utils/MassTier.java
+++ src/main/java/vg/utils/MassTier.java
@@ -3,29 +3,32 @@ package vg.utils;
 /**
  * Enum that defines possible tier of weight of entities.
  * Entities will bounce after a collision only if the other
- * entity is in a MassTier equal or higher. NOCOLLISION is
+ * entity is in a MassTier equal or higher. {@link #NOCOLLISION} is
  * a mass tier that defines entities which cannot bounce.
+ * The mass tiers are in ascending order as the ordinals of
+ * this enum may be used. (Spoiler: they are indeed)
  */
 public enum MassTier {
+
     /**
-     * HIGH : this is the mass tier for borders and bosses.
+     * NOCOLLISION : this mass tier is the lowest one
+     * and defines entities that will not bounce even if
+     * colliding with other enties in this mass tier.
      */
-    HIGH,
-    /**
-     * MEDIUM : this is the mass tier for minor enemy such
-     * as Mosquitoes.
-     */
-    MEDIUM,
+    NOCOLLISION,
     /**
      * LOW : this mass tier is not used for any Entity
      * at the present time.
      */
     LOW,
     /**
-     * NOCOLLISION : this mass tier is the lowest one
-     * and defines entities that will not bounce even if
-     * colliding with other enties in this mass tier.
+     * MEDIUM : this is the mass tier for minor enemy such
+     * as Mosquitoes.
      */
-    NOCOLLISION;
-    //TODO creare un metodo che controlli se la mass tier è maggiore uguale e diversa da NOCOLLISION
+    MEDIUM,
+    /**
+     * HIGH : this is the mass tier for borders and bosses.
+     */
+    HIGH;
+
 }
