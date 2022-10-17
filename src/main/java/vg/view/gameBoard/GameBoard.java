743f95ad0963aaf3b88f405860f986d39047dfc3
Matteo <matteo.violani@studio.unibo.it>
2022-10-17 19:49:28 +0200

    fix(gameboard): removed unused code

--- src/main/java/vg/view/gameBoard/GameBoard.java
+++ src/main/java/vg/view/gameBoard/GameBoard.java
@@ -30,30 +30,18 @@ public class GameBoard extends Application {
         AdaptableView gameView = new GameBoardView();
         viewManager = new ViewManagerImpl(stage, new KeyEventHandler());
         //this.controller = load.getController();
-        //Scene scene = new Scene(root, DimensionUtils.DEFAULT_WIDTH, DimensionUtils.DEFAULT_HEIGHT);
         this.mysteryBoxManager = new MysteryBoxManagerImpl();
 
         GameController gameController = new GameController(gameView, viewManager);
         gameView.setController(gameController);
-        //add first view to viewStack then set scene to the stage
+
+        //add first view to viewStack then set scene to the stage.
         viewManager.addScene(gameView);
-
         stage.setTitle("GameBoard");
-
-        stage.setMinWidth(950);
-        stage.setMinHeight(490);
-        stage.setMaxWidth(1400);
-        stage.setMaxHeight(950);
-//        stage.minWidthProperty().bind(scene.heightProperty().multiply(2));
-//        stage.minHeightProperty().bind(scene.widthProperty().divide(2));
-
         stage.setResizable(false);
         stage.show();
 
         this.mysteryBoxManager.initializeRound(this.controller);
-
-        viewManager = new ViewManagerImpl(stage, new KeyEventHandler());
-        viewManager.addScene(gameView);
 
         stage.setOnCloseRequest(event -> gameController.closeGame());
         gameController.gameLoop();
