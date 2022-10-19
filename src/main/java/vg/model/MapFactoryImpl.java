d0c4044583fe7354a2a122bbb8011b66b63d03ea
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-19 12:04:25 +0200

    feat(model): map factory implemented

--- src/main/java/vg/model/MapFactoryImpl.java
+++ src/main/java/vg/model/MapFactoryImpl.java
@@ -3,9 +3,11 @@ package vg.model;
 import vg.model.entity.dynamicEntity.player.Player;
 import vg.model.entity.staticEntity.StaticEntity;
 import vg.model.levels.LEVEL;
+import vg.model.levels.levelGenerator;
 import vg.model.timedObject.Bonus;
 import vg.utils.V2D;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -30,10 +32,28 @@ public class MapFactoryImpl implements MapFactory<V2D> {
     }
 
     @Override
-    public Map<V2D> fromFile() {
-        return null;
+    public Map<V2D> fromSerialized(final int lv) throws IOException, ClassNotFoundException {
+        var lg = new levelGenerator();
+        Map<V2D> map;
+        try {
+            map = lg.deserializeLevel(Integer.toString(lv));
+        } catch (IOException | ClassNotFoundException e) {
+            lg.serializeDefaults();
+            map = lg.deserializeLevel(Integer.toString(lv));
+        }
+        return map;
     }
-
+    @Override
+    public Map<V2D> fromSave() {
+        var lg = new levelGenerator();
+        Map<V2D> map;
+        try {
+            map = lg.deserializeSaved();
+        } catch (IOException | ClassNotFoundException e) {
+          throw new RuntimeException("Failed to load save file.");
+        }
+        return map;
+    }
     @Override
     public Map<V2D> fromData() {
         return null;
