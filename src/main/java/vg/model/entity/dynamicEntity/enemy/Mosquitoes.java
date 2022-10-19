059c2533ad108e296163c265232d421524425e5f
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-19 11:33:38 +0200

    feat(model): base player updated optional to guava

--- src/main/java/vg/model/entity/dynamicEntity/enemy/Mosquitoes.java
+++ src/main/java/vg/model/entity/dynamicEntity/enemy/Mosquitoes.java
@@ -6,7 +6,7 @@ import vg.utils.V2D;
 
 public class Mosquitoes extends MinorEnemy {
 
-    Mosquitoes(final V2D v2D, final V2D speed, final int radius, final Shape shape, final MassTier massTier) {
+    public Mosquitoes(final V2D v2D, final V2D speed, final int radius, final Shape shape, final MassTier massTier) {
         super(v2D, speed, radius, shape, massTier);
     }
     public void accelerate() {
