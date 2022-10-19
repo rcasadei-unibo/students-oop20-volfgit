943713ca7bf135d1286ef35b00a7f4f9dddd48e8
Matteo <matteo.violani@studio.unibo.it>
2022-10-19 14:17:43 +0200

    fix(GameBoard): commented leaderboard code, added command to close programm in stageOnClose method

--- src/main/java/vg/view/gameBoard/GameBoard.java
+++ src/main/java/vg/view/gameBoard/GameBoard.java
@@ -6,14 +6,10 @@ import vg.controller.GameController;
 import vg.controller.gameBoard.GameBoardController;
 import vg.controller.mysteryBox.manager.MysteryBoxManager;
 import vg.controller.mysteryBox.manager.MysteryBoxManagerImpl;
-import vg.controller.player.PlayerController;
-import vg.controller.player.PlayerControllerImpl;
 import vg.view.AdaptableView;
 import vg.view.ViewFactory;
 import vg.view.ViewManager;
 import vg.view.ViewManagerImpl;
-import vg.view.leaderBoard.LeaderBoardController;
-import vg.view.leaderBoard.LeaderBoardView;
 import vg.view.utils.KeyEventHandler;
 
 public class GameBoard extends Application {
@@ -34,29 +30,29 @@ public class GameBoard extends Application {
         AdaptableView<GameBoardController> gameView = ViewFactory.newGameBoardView();
         // 2) CREATE create logic controller
         GameController gameController = new GameController(gameView, viewManager);
-        PlayerController playerController = new PlayerControllerImpl();
-        playerController.setPlayerInGameArea(gameView.getViewController());
         // 3) set logic controller in view
         gameView.setIoLogicController(gameController);
 
         //4) if you want to see add it to the viewManager
         viewManager.addScene(gameView);
-
+/*
         LeaderBoardView leaderBoardView = new LeaderBoardView();
         LeaderBoardController leaderBoardController = new LeaderBoardController(leaderBoardView, viewManager);
         leaderBoardView.setIoLogicController(leaderBoardController);
 
         viewManager.addScene(leaderBoardView);
-
+*/
         stage.setTitle("GameBoard");
         stage.setResizable(false);
         stage.show();
-/*
+
         mysteryBoxManager.initializeRound(gameView.getViewController());
 
-        stage.setOnCloseRequest(event -> gameController.closeGame());
-        gameController.gameLoop();*/
-
+        stage.setOnCloseRequest(event -> {
+            gameController.closeGame();
+            System.exit(0);
+        });
+        gameController.gameLoop();
     }
 }
 
