a63759d84ddf882551c82f6857047a202d5589d9
Matteo <matteo.violani@studio.unibo.it>
2022-10-19 17:56:46 +0200

    finished method to load correctly mosquitoes in view, added new static entity metho to get view of boss and mosquitoes

--- src/main/java/vg/view/entity/StaticFactoryEntityBlock.java
+++ src/main/java/vg/view/entity/StaticFactoryEntityBlock.java
@@ -1,11 +1,26 @@
 package vg.view.entity;
 
 import javafx.geometry.Dimension2D;
+import vg.utils.V2D;
 
 public class StaticFactoryEntityBlock {
 
     public static EntityBlockImpl create(Dimension2D dimension) {
         return new EntityBlockImpl(dimension.getWidth(), dimension.getHeight());
+    }
+
+    public static EntityBlock createBoss(final V2D position) {
+        EntityBlock boss = new EntityBlockImpl(40, 40);
+        //this.boss.setImage("img/boss.png");
+        boss.setPosition(position.getVec2d());
+        return boss;
+    }
+
+    public static EntityBlock createMosquito(final V2D position) {
+        EntityBlock mosq = new EntityBlockImpl(40, 40);
+        //this.boss.setImage("img/mosquito.png");
+        mosq.setPosition(position.getVec2d());
+        return mosq;
     }
 
 }
