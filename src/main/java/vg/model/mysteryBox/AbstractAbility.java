6c3d5750cbdfde2ee5736b9dd81c77c1250537df
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2022-10-20 17:08:54 +0200

    Improvement AbstractAbility

--- src/main/java/vg/model/mysteryBox/AbstractAbility.java
+++ src/main/java/vg/model/mysteryBox/AbstractAbility.java
@@ -7,17 +7,17 @@ import vg.utils.path.PathImageMysteryBox;
 
 
 public abstract class AbstractAbility extends StaticEntity {
-    private static final double WIDTH = 60;
-    private static final double HEIGHT = 60;
+    private static final Dimension2D DIMENSION_BOX = new Dimension2D(60, 60);
+    private static final V2D INIT_POSITION = new V2D(0, 0);
 
     public final Dimension2D dimension;
     protected String pathImage;
     protected final EAbility idAbility;
 
     public AbstractAbility(EAbility idAbility) {
-        super(new V2D(0, 0), (int) (WIDTH * 2));
+        super(INIT_POSITION, (int) (DIMENSION_BOX.getWidth() * 2));
         this.pathImage = PathImageMysteryBox.MYSTERY_BOX;
-        this.dimension = new Dimension2D(WIDTH, HEIGHT);
+        this.dimension = DIMENSION_BOX;
         this.idAbility = idAbility;
     }
 
