3b69375282d96390cb37325c05a83087d94625e2
Matteo <matteo.violani@studio.unibo.it>
2022-10-21 18:54:51 +0200

    doc(GameOverView): added javadoc

--- src/main/java/vg/view/gameOver/GameOverView.java
+++ src/main/java/vg/view/gameOver/GameOverView.java
@@ -3,13 +3,8 @@ package vg.view.gameOver;
 import vg.view.AdaptableView;
 import vg.view.utils.CountdownView;
 
-public class GameOverView extends AdaptableView<GameOverViewController> implements CountdownView<GameOverViewController> {
+public class GameOverView extends AdaptableView<GameOverViewController> {
     public GameOverView() {
         super("/layout/GameOver.fxml");
-    }
-
-    @Override
-    public void setCountdown(int time) {
-        this.getViewController().setCountdown(time);
     }
 }
--- src/main/java/vg/view/gameOver/GameOverViewController.java
+++ src/main/java/vg/view/gameOver/GameOverViewController.java
@@ -1,24 +1,47 @@
 package vg.view.gameOver;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import vg.view.ViewController;
+import java.util.Optional;
 
 public class GameOverViewController extends ViewController {
 
     @FXML
-    private Text timeout;
-
-    @FXML
     private Text score;
 
-   public void setCountdown(final int time) {
-        this.timeout.setText(String.valueOf(time));
-    }
+    @FXML
+    private Text round;
 
+    @FXML
+    private TextField nameField;
+
+    /**
+     * Set score value to be showed.
+     * @param score Player score.
+     */
     public void setScore(final int score) {
         this.score.setText(String.valueOf(score));
     }
 
+    /**
+     * Set round value to be showed.
+     * @param round round when is gameover
+     */
+    public void setRound(final int round) {
+        this.round.setText(String.valueOf(round));
+    }
+
+    /**
+     * Get name typed by user.
+     * @return String of name typed by user
+     */
+    public Optional<String> getTypedName() {
+        if (this.nameField.getText().isBlank()) {
+            return Optional.empty();
+        } else {
+            return Optional.of(this.nameField.getText());
+        }
+    }
 }
