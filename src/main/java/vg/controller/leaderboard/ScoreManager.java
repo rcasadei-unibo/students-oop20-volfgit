6dbb871343de62b5cb0dbecfa2416979d935c1e8
Matteo <matteo.violani@studio.unibo.it>
2022-10-21 13:04:40 +0200

    feat(ScoreManager): added method and interface to save and load leaderboard score list

---
+++ src/main/java/vg/controller/leaderboard/ScoreManager.java
@@ -0,0 +1,30 @@
+package vg.controller.leaderboard;
+
+import vg.model.score.Score;
+import java.util.List;
+
+public interface ScoreManager {
+
+    /**
+     * Initialize ScoreManager loading data form a file if present.
+     */
+    void init();
+
+    /**
+     * Save passed score.
+     */
+    void saveScore(Score score);
+
+    /**
+     * Get top 10 score in decreasing order.
+     * @return List of top 10 scores.
+     */
+    List<Score> getTop10Score();
+
+    /**
+     * Get top score ordered.
+     * @param limit max amount of scores.
+     * @return List with "limit" amount of scores.
+     */
+    List<Score> getTopScore(int limit);
+}
