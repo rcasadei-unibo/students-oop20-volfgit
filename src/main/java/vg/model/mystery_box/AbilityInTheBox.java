875a778c2985077478ec658ec8d6db75ff31860f
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2022-10-22 20:44:04 +0200

    Add some documentation

--- src/main/java/vg/model/mystery_box/AbilityInTheBox.java
+++ src/main/java/vg/model/mystery_box/AbilityInTheBox.java
@@ -4,13 +4,30 @@ import javafx.geometry.Dimension2D;
 import vg.model.entity.Entity;
 import vg.utils.V2D;
 
-
+/**
+ * This class represents the ability of the game.
+ */
 public interface AbilityInTheBox extends Entity {
+    /**
+     *   This method is used to activate the ability.
+     *
+     */
     EAbility getIdAbility();
+
+    /**
+     *
+     * @return
+     */
     V2D getPosition();
     Dimension2D getDimension();
     String getPathImage();
     boolean isBlinking();
+
+    /**
+     *  This method is used to activate the ability.
+     *  @param isBlinking
+     */
+
 
     void setBlinking(boolean isBlinking);
 
--- src/main/java/vg/model/mystery_box/AbstractAbility.java
+++ src/main/java/vg/model/mystery_box/AbstractAbility.java
@@ -7,7 +7,9 @@ import vg.model.mystery_box.logicBlink.LogicBlinkImpl;
 import vg.utils.V2D;
 import vg.utils.path.PathImageMysteryBox;
 
-
+/**
+ * AbstractAbility is the base class for all the abilities.
+ */
 public abstract class AbstractAbility extends StaticEntity {
     private static final Dimension2D DIMENSION_BOX = new Dimension2D(60, 60);
     private static final V2D INIT_POSITION = new V2D(0, 0);
