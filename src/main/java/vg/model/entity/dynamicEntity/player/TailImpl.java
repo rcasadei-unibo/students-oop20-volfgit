acf512ef63f594f4bd19fe23751ef460a173804e
Matteo <matteo.violani@studio.unibo.it>
2022-10-23 09:57:28 +0200

    fix(TailImpl): removed method to convert list of point to a list of vertex

--- src/main/java/vg/model/entity/dynamicEntity/player/TailImpl.java
+++ src/main/java/vg/model/entity/dynamicEntity/player/TailImpl.java
@@ -3,6 +3,8 @@ package vg.model.entity.dynamicEntity.player;
 import com.google.common.base.Optional;
 import vg.utils.Direction;
 import vg.utils.V2D;
+import vg.utils.V2DUtility;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
@@ -48,39 +50,7 @@ public final class TailImpl implements Tail {
 
     @Override
     public List<V2D> getVertex() {
-        List<V2D> vertex = new ArrayList<>();
-        if (!this.coordinates.isEmpty()) {
-            Iterator<V2D> it = this.coordinates.iterator();
-            Direction prevDir = Direction.NONE;
-            V2D prev = it.next();
-            vertex.add(prev);
-            while (it.hasNext()) {
-                V2D next = it.next();
-                //if prev direction and current are different, it means that
-                // direction changed so "next" V2D is a vector
-                Direction diffDir = getDirection(next.diff(prev));
-                if (prevDir != diffDir) {
-                    vertex.add(next.diff(next.diff(prev)));
-                    prevDir = diffDir;
-                }
-                prev = next;
-            }
-        }
-        return vertex;
+        return V2DUtility.getVertex(this.coordinates);
     }
 
-    private Direction getDirection(final V2D vector) {
-        V2D vec = vector.getSingVector();
-        if (vec.equals(Direction.DOWN.getVector())) {
-            return Direction.DOWN;
-        } else if (vec.equals(Direction.UP.getVector())) {
-            return Direction.UP;
-        } else if (vec.equals(Direction.LEFT.getVector())) {
-            return Direction.LEFT;
-        } else if (vec.equals(Direction.RIGHT.getVector())) {
-            return Direction.RIGHT;
-        } else {
-            return Direction.NONE;
-        }
-    }
 }
