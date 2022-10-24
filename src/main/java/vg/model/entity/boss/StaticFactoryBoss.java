329cf0acd5e9bc5b2b5ae38be4eb07b72f20fc12
j3yxz <alexandru.bragari@studio.unibo.it>
2022-10-24 13:29:03 +0200

    fix(model): base speed of boss set to 1,1

--- src/main/java/vg/model/entity/boss/StaticFactoryBoss.java
+++ src/main/java/vg/model/entity/boss/StaticFactoryBoss.java
@@ -5,6 +5,6 @@ import vg.utils.V2D;
 public class StaticFactoryBoss {
 
     public static BossModel createRound1() {
-        return new BossImpl(new V2D(2, 2));
+        return new BossImpl(new V2D(1, 1));
     }
 }
