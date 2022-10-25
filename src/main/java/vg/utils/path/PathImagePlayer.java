b9b363991689adf9f869059698ea2e3513d2346e
Luana Mennuti <luigina.mennuti2@studio.unibo.it>
2022-10-25 16:05:10 +0200

    End checkstyle, pmd, spotbugs for utilities classesss.

--- src/main/java/vg/utils/path/PathImagePlayer.java
+++ src/main/java/vg/utils/path/PathImagePlayer.java
@@ -2,10 +2,16 @@ package vg.utils.path;
 
 import java.util.List;
 
-public class PathImagePlayer {
+/**
+ * Utility class for path of player.
+ */
+public final class PathImagePlayer {
     private static final String PATH_RADICE = "img/";
     private static final String PATH_PLAYER = PATH_RADICE + "player/";
 
+    /**
+     * Provides the path of the image of the player, for the animation.
+     */
     public static final List<String> PLAYER = List.of(
             PATH_PLAYER + "player1.png",
             PATH_PLAYER + "player1.png",
@@ -15,8 +21,10 @@ public class PathImagePlayer {
             PATH_PLAYER + "player2.png"
     );
 
+    /**
+     * Provides the path of the image of the shield player.
+     */
     public static final String SHIELD = PATH_RADICE + "shield.png";
-
-
-
+    private PathImagePlayer() {
+    }
 }
--- src/test/java/vg/utils/path/PathImageMysteryBoxTest.java
+++ src/test/java/vg/utils/path/PathImageMysteryBoxTest.java
@@ -11,8 +11,6 @@ class PathImageMysteryBoxTest {
         assertEquals("img/mysteryBox/coins/coin-T.png", PathImageMysteryBox.COIN_TIME);
         assertEquals("img/mysteryBox/coins/coin-S.png", PathImageMysteryBox.COIN_SPEED);
         assertEquals("img/mysteryBox/coins/coin-P.png", PathImageMysteryBox.COIN_SCORE);
-        assertEquals("img/mysteryBox/coins/coin-Boom.png", PathImageMysteryBox.COIN_WEAPON_BOSS);
-        assertEquals("img/mysteryBox/coins/coin-L.png", PathImageMysteryBox.COIN_WEAPON_MOQUETOES);
         assertEquals("img/mysteryBox/coins/coin-C.png", PathImageMysteryBox.COIN_KILL_ALL_MOQUETOES);
     }
 
